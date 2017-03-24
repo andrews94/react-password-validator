55fe46d42f14b6b4231208418feab59dac12d73d
Aaron <andrews_aaron@bah.com>
2017-03-24 15:59:42 -0400

    added all code to one page (validator.js)

--- src/App.js
+++ src/App.js
@@ -2,45 +2,55 @@ import React, { Component } from 'react';
 import Validator from "./Validator"
 
 class Login extends Component {
-  constructor(props) {
-    super()
-    this.state = {
-      user: "",
-      password: "",
-      validator: ""
-    };
-    this.validUser = this.validUser.bind(this);
-    this.validPassword = this.validPassword.bind(this);
-    this.validValidator = this.validValidator.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+  render() {
+    return(
+      <Validator />
+    )
   }
-  validUser(lit) {
-    this.setState({user: lit.target.value})
-  }
-  validPassword(lit) {
-    this.setState({password: lit.target.value})
-  }
-  validValidator(lit) {
-    this.setState({validator: lit.target.value})
-  }
-  handleSubmit(lit) {
-    lit.preventDefault();
-    if(this.state.password === this.state.validator) {
-      alert("Logged in. Welcome...");
-    } else {
-      alert("Failure Login...");
-    };
-  }
-render() {
-  return (
-    <form onSubmit={this.handleSubmit}>
-      <h1>Sign Up</h1>
-      <input type="text" value={this.state.user} placeholder="Enter Email" onChange={this.validUser}/>
-      <input type="password" value={this.state.password} placeholder="Enter Password" onChange={this.validPassword} />
-      <input type="password" value={this.state.validator} placeholder="Re-Enter Password" onChange={this.validValidator}/>
-      <input type="submit" value="Submit" />
-      </form>
-      );
-    }
-  }
-export default Login
+}
+
+export default Login;
+
+// class Login extends Component {
+//   constructor(props) {
+//     super()
+//     this.state = {
+//       user: "",
+//       password: "",
+//       validator: ""
+//     };
+//     this.validUser = this.validUser.bind(this);
+//     this.validPassword = this.validPassword.bind(this);
+//     this.validValidator = this.validValidator.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+//   validUser(litty) {
+//     this.setState({user: litty.target.value})
+//   }
+//   validPassword(litty) {
+//     this.setState({password: litty.target.value})
+//   }
+//   validValidator(litty) {
+//     this.setState({validator: litty.target.value})
+//   }
+//   handleSubmit(litty) {
+//     litty.preventDefault();
+//     if(this.state.password === this.state.validator) {
+//       alert("Logged in. Welcome...");
+//     } else {
+//       alert("Failure Login...");
+//     };
+//   }
+// render() {
+//   return (
+//     <form onSubmit={this.handleSubmit}>
+//       <h1>Sign Up</h1>
+//       <input type="text" value={this.state.user} placeholder="Enter Email" onChange={this.validUser}/>
+//       <input type="password" value={this.state.password} placeholder="Enter Password" onChange={this.validPassword} />
+//       <input type="password" value={this.state.validator} placeholder="Re-Enter Password" onChange={this.validValidator}/>
+//       <input type="submit" value="Submit" />
+//       </form>
+//       );
+//     }
+//   }
+// export default Login
--- src/validator.js
+++ src/validator.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
 import "./Validator.css";
 
-class Validator extends Component {
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <h1>Sign Up</h1>
-        <input type="text" value={this.state.user} placeholder="Enter Email" onChange={this.validUser}/>
-        <input type="text" placeholder="Enter Password" onChange={this.validPassword} />
-        <input type="text" placeholder="Re-Enter Password" onChange={this.validValidator}/>
-        <input type="submit" value="Submit" />
-        </form>
-    );
-  }
-}
 
+class Validator extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      user: "",
+      password: "",
+      validator: ""
+    };
+    this.validUser = this.validUser.bind(this);
+    this.validPassword = this.validPassword.bind(this);
+    this.validValidator = this.validValidator.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  validUser(check) {
+    this.setState({user: check.target.value})
+  }
+  validPassword() {
+    this.setState({password: check.target.value})
+  }
+  validValidator(check) {
+    this.setState({validator: check.target.value})
+  }
+  handleSubmit(check) {
+    check.preventDefault();
+    if(this.state.password === this.state.validator) {
+      alert("Logged in. Welcome...");
+    } else {
+      alert("Failure Login...");
+    };
+  }
+render() {
+  return (
+    <form onSubmit={this.handleSubmit}>
+      <h1>Sign Up</h1>
+      <input type="text" value={this.state.user} placeholder="Enter Email" onChange={this.validUser}/>
+      <input type="password" value={this.state.password} placeholder="Enter Password" onChange={this.validPassword} />
+      <input type="password" value={this.state.validator} placeholder="Re-Enter Password" onChange={this.validValidator}/>
+      <input type="submit" value="Submit" />
+      </form>
+      );
+    }
+  }
 export default Validator;
